72de8f9ad958668b85737246fa7e79aec33e1def
jung-dayoon <jung.dayoon@worksmobile.com>
2022-01-11 18:11:00 +0900

    network 관련 추가 - Search

--- app/src/main/java/com/example/rxjavastudy/network/GiphyApiClient.kt
+++ app/src/main/java/com/example/rxjavastudy/network/GiphyApiClient.kt
@@ -1,16 +1,18 @@
 package com.example.rxjavastudy.network
 
 import com.example.rxjavastudy.Constants
-import com.example.rxjavastudy.data.Giphy
 import io.reactivex.Single
-import retrofit2.Response
 import javax.inject.Inject
 
 class GiphyApiClient @Inject constructor (
     private val giphyApi: GiphyApi
 ) {
-    fun getRandomGiphy(): Single<Response<GiphyRandomResponse>> {
+    fun getRandomGiphy(): Single<GiphyRandomResponse> {
         return giphyApi.getRandomGif(Constants.apiKey, null, null, null)
+    }
+
+    fun getSearchGiphyList(searchQuery: String, limit: Int, offset: Int): Single<GiphySearchResponse> {
+        return giphyApi.getSearchGifList(Constants.apiKey, searchQuery, limit, offset)
     }
 }
 
